0459560c664867a842c73969c43d80b7bd8fed0b
Peter Georgiev <peroxmen@hotmail.com>
2017-04-05 08:44:42 +0300

    Add Programming.Basics.Lab-16.August.2015.sln

---
+++ Programming.Basics.Lab-16.August.2015/01.BaiIvanAdventures/BaiIvanAdventures.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class BaiIvanAdventures
+{
+    static void Main(string[] args)
+    {
+        int dayOfWeek = int.Parse(Console.ReadLine());
+        decimal availableMoney = decimal.Parse(Console.ReadLine());
+        decimal desiredAmountOfAlcohol = decimal.Parse(Console.ReadLine());
+
+        decimal boughtAlcohol = 0m;
+        switch (dayOfWeek)
+        {
+            case 0: boughtAlcohol = availableMoney / 25.0m; break;
+            case 1: boughtAlcohol = availableMoney / 21.0m; break;
+            case 2: boughtAlcohol = availableMoney / 14.0m; break;
+            case 3: boughtAlcohol = availableMoney / 17.0m; break;
+            case 4: boughtAlcohol = availableMoney / 45.0m; break;
+            case 5: boughtAlcohol = availableMoney / 59.0m; break;
+            case 6: boughtAlcohol = availableMoney / 42.0m; break;
+            default:
+                break;
+        }
+
+        string status = "sober";
+        if (boughtAlcohol > 1.5m)
+        {
+            status = "very drunk";
+        }
+        else if (boughtAlcohol >= 1.0m)
+        {
+            status = "drunk";
+        }
+
+        if (desiredAmountOfAlcohol < boughtAlcohol)
+        {
+            Console.WriteLine($"Bai Ivan is {status} and very happy and he shared {boughtAlcohol - desiredAmountOfAlcohol:F2} l. of alcohol with his friends");
+        }
+        else if (desiredAmountOfAlcohol == boughtAlcohol)
+        {
+            Console.WriteLine($"Bai Ivan is {status} and happy. He is as drunk as he wanted");
+        }
+        else
+        {
+            Console.WriteLine($"Bai Ivan is {status} and quite sad. He wanted to drink another {desiredAmountOfAlcohol - boughtAlcohol:F2} l. of alcohol");
+        }
+    }
+}
---
+++ Programming.Basics.Lab-16.August.2015/04.PhoneProcesses/PhoneProcesses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class PhoneProcesses
+{
+    static void Main()
+    {
+        string batteryInput = Console.ReadLine();
+        int batteryState = int.Parse(batteryInput.Remove(batteryInput.Length - 1));
+        string endOfInput = "end";
+        string input = "";
+
+        List<int> appRequirements = new List<int>();
+
+        while ((input = Console.ReadLine().ToLower()) != endOfInput)
+        {
+            int battery = int.Parse(input.Substring(input.Length - 3, 2));
+            appRequirements.Add(battery);
+        }
+        int currentIndex = 0;
+        while (currentIndex <= appRequirements.Count)
+        {
+            bool phoneOff = batteryState <= 0;
+            bool needsRecharge = batteryState <= 15;
+            bool insideRange = currentIndex < appRequirements.Count;
+            if (phoneOff)
+            {
+                Console.WriteLine("Phone Off"); break;
+            }
+            else if (currentIndex >= appRequirements.Count && !needsRecharge)
+                Console.WriteLine("Successful complete -> {0}%", batteryState);
+            else if (needsRecharge)
+            {
+                Console.WriteLine("Connect charger -> {0}%", batteryState);
+                Console.WriteLine("Programs left -> {0}", appRequirements.Count - currentIndex);
+                break;
+            }
+            else if (insideRange)
+                batteryState -= appRequirements[currentIndex];
+
+            currentIndex++;
+        }
+    }
+}
